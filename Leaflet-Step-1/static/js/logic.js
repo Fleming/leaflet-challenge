1771be59f4e1bf4ec94921dd9abf05170717d335
Fleming <47439078+Fleming@users.noreply.github.com>
2021-05-23 22:14:03 -0400

    Update logic.js

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -2,3 +2,9 @@
 
 const link = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_week.geojson';
 
+// perform a GET request to the query URL
+d3.json(link).then((data) => {
+    // Once we get a response, send the data.features object to the createFeatures function
+    createFeatures(data.features);
+    console.log(data.features);
+});
